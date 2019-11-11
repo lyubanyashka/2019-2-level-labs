beb68fd4133d6056652552fea1d47bb4c7c27ff8
lyubanyashka <lyubanyashka@yandex.ru>
2019-11-11 18:54:46 +0300

    final

--- lab_2/test.py
+++ lab_2/test.py
@@ -14,7 +14,7 @@ class PrintPathTest(unittest.TestCase):
     Tests path finding
     """
 
-    def test_find_path_ideal_again(self):
+    def test_find_pathl_again(self):
         """
         Ideal scenario # 2
         """
@@ -24,9 +24,9 @@ class PrintPathTest(unittest.TestCase):
         add_weight = 1
         remove_weight = 2
         substitute_weight = 3
-        matrix = tuple(main.generate_edit_matrix(len(original_word) + 1, len(target_word) + 1))
-        matrix = tuple(main.initialize_edit_matrix(matrix, add_weight, remove_weight))
-        matrix = main.fill_edit_matrix(matrix, add_weight, remove_weight, substitute_weight, original_word, target_word)
-        print("best path cost " + str(matrix[len(original_word)][len(target_word)]))
-        main.print_path(matrix, original_word, target_word, add_weight, remove_weight, substitute_weight)
+        edit_matrix = tuple(main.generate_edit_matrix(len(original_word) + 1, len(target_word) + 1))
+        edit_matrix = tuple(main.initialize_edit_matrix(edit_matrix, add_weight, remove_weight))
+        edit_matrix = main.fill_edit_matrix(edit_matrix, add_weight, remove_weight, substitute_weight, original_word, target_word)
+        print("best path cost " + str(edit_matrix[len(original_word)][len(target_word)]))
+        main.print_path(edit_matrix, original_word, target_word, add_weight, remove_weight, substitute_weight)
 
